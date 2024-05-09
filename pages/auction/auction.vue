92780b87b063ebbfceaa5596046fbdcc9ff2d22b
alin13c <1498669420@qq.com>
2024-05-10 01:25:04 +0800

    Create auction.vue

---
+++ pages/auction/auction.vue
@@ -0,0 +1,25 @@
+<template>
+	 <view class="">
+	 	<web-view src="/hybrid/html/local.html"></web-view>
+	 </view>
+</template>
+
+<script>
+	export default {
+		data() {
+			return {
+				
+			}
+		},
+		onLoad() {
+
+		},
+		methods: {
+			
+		}
+	}
+</script>
+
+<style>
+	
+</style>
