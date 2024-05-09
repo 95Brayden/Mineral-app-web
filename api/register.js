1cc09b7934a8c234402059bbb4e3a980a52f6042
alin13c <1498669420@qq.com>
2024-05-09 14:52:37 +0800

    Create register.js

---
+++ api/register.js
@@ -0,0 +1,19 @@
+import request from '@/utils/requestUtil'
+
+export function memberRegister(data) {
+	return request({
+		method: 'POST',
+		url: '/sso/register',
+		header: {
+			'content-type': 'application/x-www-form-urlencoded;charset=utf-8'
+		},
+		data: data
+	})
+}
+
+export function memberInfo() {
+	return request({
+		method: 'GET',
+		url: '/sso/info'
+	})
+}
