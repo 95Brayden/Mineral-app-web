7b77fc81326d474fee04a7a11b222f5363f27788
alin13c <1498669420@qq.com>
2024-05-11 18:23:04 +0800

    Update start.vue

--- pages/start/start.vue
+++ pages/start/start.vue
@@ -1,70 +1,208 @@
 <template>
-	
-  <view class="startup-page">
-    <img src="@/static/start.jpg" alt="Logo">
-    <h1>Welcome to Mineral</h1>
-    <p>Loading...</p>
-	<button @click="redirectToIndex()">跳转到首页</button>
-	<view class="cate-item" @click="navToCommodityList()">
-		<image src="/static/temp/c3.png"></image>
-	</view>
-	<view class="cate-item" @click="navToIndex()">
-		<image src="/static/temp/c3.png"></image>
-	</view>
+  <view>
+	  <view class="guess-section">
+	  	<view v-for="(item, index) in recommendProductList" :key="index" class="guess-item" @click="navToDetailPage(item)">
+	  		<view class="image-wrapper">
+	  			<image :src="item.pic" mode="aspectFill"></image>
+	  		</view>
+	  		<text class="title clamp">{{item.name}}</text>
+	  		<text class="title2 clamp">{{item.subTitle}}</text>
+	  		<text class="price">￥{{item.price}}</text>
+	  	</view>
+	  </view>
+	  <uni-load-more :status="loadingType"></uni-load-more>
   </view>
+  
 </template>
 
 <script>
-	import empty from "@/components/empty";
-	import uniLoadMore from '@/components/uni-load-more/uni-load-more.vue';
+	import {
+		fetchContent,
+		fetchRecommendProductList
+	} from '@/api/home.js';
 	import {
 		formatDate
 	} from '@/utils/date';
+	import uniLoadMore from '@/components/uni-load-more/uni-load-more.vue';
 	export default {
 		components: {
-			uniLoadMore,
-			empty
+			uniLoadMore	
+		},
+		data() {
+			return {
+				titleNViewBackground: '',
+				titleNViewBackgroundList: ['rgb(203, 87, 60)', 'rgb(205, 215, 218)'],
+				swiperCurrent: 0,
+				swiperLength: 0,
+				carouselList: [],
+				goodsList: [],
+				advertiseList: [],
+				advertiseList2: [
+					{ pic: '/static/carousel/carousel1.jpg' },
+					{ pic: '/static/carousel/carousel2.png' },
+					{ pic: '/static/carousel/carousel3.png' },
+					{ pic: '/static/carousel/cartier.png' },
+					{ pic: '/static/carousel/gold.jpg' },
+				],
+				brandList: [],
+				homeFlashPromotion: [],
+				newProductList: [],
+				hotProductList: [],
+				recommendProductList: [],
+				recommendParams: {
+					pageNum: 1,
+					pageSize: 4
+				},
+				loadingType:'more'
+			};
+		},
+		onLoad() {
+			this.loadData();
+		},
+		//下拉刷新
+		onPullDownRefresh(){
+			console.log("run");
+			this.recommendParams.pageNum=1;
+			this.loadData();
+		},
+		//加载更多
+		onReachBottom(){
+			this.recommendParams.pageNum++;
+			this.loadingType = 'loading';
+			fetchRecommendProductList(this.recommendParams).then(response => {
+				let addProductList = response.data;
+				if(response.data.length===0){
+					//没有更多了
+					this.recommendParams.pageNum--;
+					this.loadingType = 'nomore';
+				}else{
+					this.recommendProductList = this.recommendProductList.concat(addProductList);
+					this.loadingType = 'more';
+				}
+			})
+		},
+		computed: {
+			cutDownTime() {
+				let endTime = new Date(this.homeFlashPromotion.endTime);
+				let endDateTime = new Date();
+				let startDateTime = new Date();
+				endDateTime.setHours(endTime.getHours());
+				endDateTime.setMinutes(endTime.getMinutes());
+				endDateTime.setSeconds(endTime.getSeconds());
+				let offsetTime = (endDateTime.getTime() - startDateTime.getTime());
+				let endHour = Math.floor(offsetTime / (60 * 60 * 1000));
+				let offsetMinute = offsetTime % (60 * 60 * 1000);
+				let endMinute = Math.floor(offsetMinute / (60 * 1000));
+				let offsetSecond = offsetTime % (60 * 1000);
+				let endSecond = Math.floor(offsetSecond / 1000);
+				return {
+					endHour: endHour,
+					endMinute: endMinute,
+					endSecond: endSecond
+				}
+			}
+		},
+		filters: {
+			formatTime(time) {
+				if (time == null || time === '') {
+					return 'N/A';
+				}
+				let date = new Date(time);
+				return formatDate(date, 'hh:mm:ss')
+			},
 		},
 		methods: {
-			navToIndex(){
-				try {
-				    uni.navigateTo({
-				      url: `/pages/index/index`
-				    });
-				  } catch (error) {
-				    console.error('跳转失败', error);
-				  }
+			/**
+			 * 加载数据
+			 */
+			async loadData() {
+				fetchContent().then(response => {
+					console.log("onLoad", response.data);
+					this.advertiseList = response.data.advertiseList;
+					this.swiperLength = this.advertiseList.length;
+					this.titleNViewBackground = this.titleNViewBackgroundList[0];
+					this.brandList = response.data.brandList;
+					this.homeFlashPromotion = response.data.homeFlashPromotion;
+					this.newProductList = response.data.newProductList;
+					this.hotProductList = response.data.hotProductList;
+					fetchRecommendProductList(this.recommendParams).then(response => {
+						this.recommendProductList = response.data;
+						uni.stopPullDownRefresh();
+					})
+				});
 			},
-			navToCommodityList() {
+			navToDetailPage(item) {
+				let id = item.id;
 				uni.navigateTo({
-					url: `/pages/product/product`
+					url: `/pages/product/product?id=${id}`
 				})
 			},
 		},
 	}
+	
 </script>
 
-<style>
-.startup-page {
-  display: flex;
-  flex-direction: column;
-  justify-content: center;
-  align-items: center;
-  height: 100vh;
-}
+<style lang="scss">
+.guess-section {
+		display: flex;
+		flex-wrap: wrap;
+		padding: 0 30upx;
+		background: #fff;
 
-.startup-page img {
-  width: 500px;
-  height: 500px;
-}
+		.guess-item {
+			display: flex;
+			flex-direction: column;
+			width: 48%;
+			padding-bottom: 40upx;
 
-.startup-page h1 {
-  font-size: 24px;
-  margin-top: 20px;
-}
+			&:nth-child(2n+1) {
+				margin-right: 4%;
+			}
+		}
 
-.startup-page p {
-  font-size: 16px;
-  margin-top: 10px;
-}
+		.image-wrapper {
+			width: 100%;
+			height: 330upx;
+			border-radius: 3px;
+			overflow: hidden;
+
+			image {
+				width: 100%;
+				height: 100%;
+				opacity: 1;
+			}
+		}
+		
+		.image-wrapper-brand {
+			width: 100%;
+			height: 150upx;
+			border-radius: 3px;
+			overflow: hidden;
+		
+			image {
+				width: 100%;
+				height: 100%;
+				opacity: 1;
+			}
+		}
+
+		.title {
+			font-size: $font-lg;
+			color: $font-color-dark;
+			line-height: 80upx;
+		}
+
+		.title2 {
+			font-size: $font-sm;
+			color: $font-color-light;
+			line-height: 40upx;
+		}
+
+		.price {
+			font-size: $font-lg;
+			color: $uni-color-primary;
+			line-height: 1;
+		}
+	}
+
 </style>
