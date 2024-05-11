4af644ee84eb379aa2ecd70b9fa2dde196520610
alin13c <1498669420@qq.com>
2024-05-11 17:05:36 +0800

    Update appConfig.js

--- utils/appConfig.js
+++ utils/appConfig.js
@@ -1,6 +1,6 @@
 // appConfig.js
 
 //配置API请求的基础路径
-export const API_BASE_URL = 'http://47.99.95.4:8201/portal';
+export const API_BASE_URL = 'http://47.99.95.4:8201/mall-portal';
 //是否启用支付宝支付
 export const USE_ALIPAY = false;
