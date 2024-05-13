6571b3a91d205b8fcf0c336736c284639fb10431
alin13c <1498669420@qq.com>
2024-05-13 22:19:47 +0800

    Update searchfalse.vue

--- pages/start/searchfalse.vue
+++ pages/start/searchfalse.vue
@@ -1,7 +1,6 @@
 <template>
-  <view>
-	  <text class="title">没有搜索到相关物品</text>
-	  <uni-load-more :status="loadingType"></uni-load-more>
+  <view class="container">
+	   <text class="text-nothing">没有搜索到相关物品</text>
   </view>
   
 </template>
@@ -19,70 +18,13 @@
 </script>
 
 <style lang="scss">
-.guess-section {
-		display: flex;
-		flex-wrap: wrap;
-		padding: 0 30upx;
-		background: #fff;
-
-		.guess-item {
+		.text-nothing {
+			font-size: 28upx;
+			color: #999
+		}
+			
+		.container{
 			display: flex;
-			flex-direction: column;
-			width: 48%;
-			padding-bottom: 40upx;
-
-			&:nth-child(2n+1) {
-				margin-right: 4%;
-			}
-		}
-
-		.image-wrapper {
-			width: 100%;
-			height: 330upx;
-			border-radius: 3px;
-			overflow: hidden;
-
-			image {
-				width: 100%;
-				height: 100%;
-				opacity: 1;
-			}
-		}
-		
-		.image-wrapper-brand {
-			width: 100%;
-			height: 150upx;
-			border-radius: 3px;
-			overflow: hidden;
-		
-			image {
-				width: 100%;
-				height: 100%;
-				opacity: 1;
-			}
-		}
-
-		.title {
-			font-size: $font-lg;
-			color: $font-color-dark;
-			line-height: 80upx;
-		}
-
-		.title2 {
-			font-size: $font-sm;
-			color: $font-color-light;
-			line-height: 40upx;
-		}
-
-		.price {
-			font-size: $font-lg;
-			color: $uni-color-primary;
-			line-height: 1;
-		}
-	}
-.title {
-			font-size: $font-lg;
-			color: $font-color-dark;
-			line-height: 80upx;
+			justify-content: center;
 		}
 </style>
